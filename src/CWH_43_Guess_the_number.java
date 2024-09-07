1fdae4d80011a8cac78ba0672da7c9a3fe5bcc0b
v Koushik <165685307+Koushikv18@users.noreply.github.com>
2024-09-07 17:07:14 +0530

    Update CWH_43_Guess_the_number.java

--- src/CWH_43_Guess_the_number.java
+++ src/CWH_43_Guess_the_number.java
@@ -1,5 +1,5 @@
-/* Create a class Game, which allows a user to play "Guess the Number" game once.
-       Game should have the following methods:
+/* Creating a class Game, which allows a user to play "Guess the Number" game once.
+       Game will have the following methods:
        1. Constructor to generate the random number
        2. takeUserInput() to take a user input of number
        3. isCorrectNumber() to detect whether the number entered by user is true
